f0e62e8496b5b084253a7d4a8d6979649484e895
Chris Welter <xryswelter@gmail.com>
2018-10-31 13:36:15 -0700

    updating js with array of objects for button creation js

---
+++ resources/projects/Assets/JavaScript/projects.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  loadbuttons();
+});
+
+let buttonArray = [
+  {
+    name: "bamazon",
+    text: "bamazon is a project to demonstrate Node and MySQL skills",
+    github: "-",
+    link: "https://github.com/xryswelter/bamazon",
+    image: "../Images/commercial-amazon.jpg",
+    value = 0
+  },
+  {
+    name: "Gems of Fate",
+    text: "Gems of Fate is a game application to demonstrate my html, css, and JQuery skills",
+    link: "https://xryswelter.github.io/gems-of-fate/",
+    github: "https://github.com/xryswelter/gems-of-fate",
+    image: "../Images/",
+    value = 1
+  },
+  {
+    name: "Train Game",
+    text: "Train Game is ",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 2
+  },
+  {
+    name: "",
+    text: "",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 0
+  },
+  {
+    name: "",
+    text: "",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 0
+  },
+  {
+    name: "",
+    text: "",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 0
+  },
+  {
+    name: "",
+    text: "",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 0
+  },
+  {
+    name: "",
+    text: "",
+    link: "",
+    github: "",
+    image: "../Images/",
+    value = 0
+  }
+];
