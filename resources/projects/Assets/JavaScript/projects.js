353504dd59640614b696b5d692dad3cf28b79257
Chris Welter <xryswelter@gmail.com>
2018-11-02 13:21:09 -0700

    intro prototype of resume page

--- resources/projects/Assets/JavaScript/projects.js
+++ resources/projects/Assets/JavaScript/projects.js
@@ -1,84 +1,136 @@
-$(document).ready(function() {
-  loadbuttons();
-});
-
+// Array for button creation
 let buttonArray = [
   {
     name: "bamazon",
+    title: "bamazon",
     text: "bamazon is a project to demonstrate Node and MySQL skills",
     github: "-",
     link: "https://github.com/xryswelter/bamazon",
-    image: "../Images/commercial-amazon.jpg",
-    value = 0
+    image: "./resources/projects/Assets/Images/commercial-amazon.jpg",
+    value: "0"
   },
   {
     name: "Gems of Fate",
-    text: "Gems of Fate is a game application to demonstrate my html, css, and JQuery skills",
+    title: "gems",
+    text:
+      "Gems of Fate is a game application to demonstrate my html, css, and JQuery skills",
     link: "https://xryswelter.github.io/gems-of-fate/",
     github: "https://github.com/xryswelter/gems-of-fate",
-    image: "../Images/Citrine-November.png",
-    value = 1
+    image: "./resources/projects/Assets/Images/Citrine-November.png",
+    value: "1"
   },
   {
     name: "Train Game",
-    text: "This webpage was created to demonstrate my skill with API keys using Firebase as a database to create a simulation of a train schedule.",
+    title: "train",
+    text:
+      "This webpage was created to demonstrate my skill with API keys using Firebase as a database to create a simulation of a train schedule.",
     link: "https://xryswelter.github.io/TrainGame/",
     github: "https://github.com/xryswelter/TrainGame",
-    image: "../Images/train.png",
-    value = 2
+    image: "./resources/projects/Assets/Images/train.png",
+    value: "2"
   },
   {
     name: "Trivia Game",
-    text: "This is a webpage to demonstrate my use of jQuery and the use of the display/hide functionality of CSS using jQuery and html/css.",
+    title: "trivia",
+    text:
+      "This is a webpage to demonstrate my use of jQuery and the use of the display/hide functionality of CSS using jQuery and html/css.",
     link: "https://xryswelter.github.io/Trivia-Game/",
     github: "https://github.com/xryswelter/Trivia-Game",
-    image: "../Images/Trivia.png",
-    value = 3
+    image: "./resources/projects/Assets/Images/Trivia.png",
+    value: "3"
   },
   {
     name: "Giphy",
-    text: "This app is to demonstrate the use of API Keys in conjuction with creating images that have multiple states dependent on the JSON return value.",
+    title: "giphy",
+    text:
+      "This app is to demonstrate the use of API Keys in conjuction with creating images that have multiple states dependent on the JSON return value.",
     link: "https://xryswelter.github.io/GIPHY-project/",
     github: "https://github.com/xryswelter/GIPHY-project",
-    image: "../Images/giffy.jpg",
-    value = 4
+    image: "./resources/projects/Assets/Images/giffy.jpg",
+    value: "4"
   },
   {
     name: "Music Venue Search",
-    text: "This is a music venue search that will display upcoming music venue locations, names, tickets, and maps for a specific artist or band.",
+    title: "project1",
+    text:
+      "This is a music venue search that will display upcoming music venue locations, names, tickets, and maps for a specific artist or band.",
     link: "https://christiehennes.github.io./Project1/",
     github: "https://github.com/christiehennes/Project1",
-    image: "../Images/project1.jpg",
-    value = 5
+    image: "./resources/projects/Assets/Images/project1.jpg",
+    value: "5"
   },
   {
     name: "Spielkatalog",
-    text: "Spielkatalog is a simple, modern tool to help track and organize your video game collection.",
+    title: "project2",
+    text:
+      "Spielkatalog is a simple, modern tool to help track and organize your video game collection.",
     link: "http://spielkatalog.herokuapp.com/",
     github: "https://github.com/xryswelter/Spielkatalog",
-    image: "../Images/spielkatalog.PNG",
-    value = 6
+    image: "./resources/projects/Assets/Images/spielkatalog.PNG",
+    value: "6"
   },
   {
     name: "CPR_finalproject",
-    text: "An application that was designed to help those wishing to get a CPR certification by providing the online CPR test.",
+    title: "project3",
+    text:
+      "An application that was designed to help those wishing to get a CPR certification by providing the online CPR test.",
     link: "https://secret-wave-42323.herokuapp.com",
     github: "https://github.com/kkirker/CPR_finalproject",
-    image: "../Images/cpr.jpg",
-    value = 7
+    image: "./resources/projects/Assets/Images/cpr.jpg",
+    value: "7"
   }
 ];
-
-function loadbuttons(){
-    buttonArray.forEach(element => {
-        let button = document.createElement("BUTTON");
-        button.addClass(`${name}`).value(`${value}`);
-        button.setAttribute('style', `background-image:${image}`);
-        if(value>5){
-            $('.homeworks').appendChild(button)
-        } else{
-            $('.projects').appendChild(button)
-        }
-    });
+//Function to load buttons onto screen
+function loadbuttons() {
+  buttonArray.forEach(element => {
+    let button = document.createElement("BUTTON");
+    button.classList.add(`${element.title}`);
+    button.setAttribute("id", "button");
+    button.setAttribute("value", `${element.value}`);
+    button.setAttribute("style", `background-image: url("${element.image}")`);
+    // button.addEventListener('click',display(event))
+    button.innerHTML = `${element.name}`;
+    if (element.value < 5) {
+      $(".homeworks").append(button);
+    } else {
+      $(".projects").append(button);
+    }
+  });
 }
-
+// Document onload function
+$(document).ready(function() {
+  loadbuttons();
+  console.log('echo')
+});
+// Click Event to display information below buttons
+$('#button').on('click', function () {
+  console.log("zoo");
+  display();
+});
+// Displaying function
+function display() {
+  // event.preventDefault()
+  console.log("moo");
+  // clear();
+  console.log("poo");
+  let value = this.val();
+  let buttonEvent = buttonArray[value];
+  $(".name").innerHTML(`${buttonEvent.name}`);
+}
+// Reset info function
+function clear() {
+}
+{
+  /* <div class="explanation">
+  <div class="row">
+                <H2 class="name"></H2>
+                <h3 class="text"></h3>
+                <a class="link">
+                <button> Launch Application?</button>
+                </a>
+                <a class="github">
+                <button> See github repo?</button>
+                </a>
+                </div>
+                </div> */
+}
